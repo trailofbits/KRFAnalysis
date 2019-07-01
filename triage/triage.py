ec0ea8d44728804baaf479e7e6e8de013229640e
Henry Wildermuth <hmwildermuth@gmail.com>
2019-07-01 16:42:04 -0400

    triage: Update triage script for new LLVM pass output

--- triage/triage.py
+++ triage/triage.py
@@ -104,52 +104,46 @@ def severity(call):
     return UNK
 
 
-def formatCallData(data, func, module):
+def formatCallData(data):
     s = data["call"]
-    s += " in " + func + "()"
+    s += " in " + data["function"] + "()"
     if "line" in data and "file" in data and "dir" in data:
         s += " @ " + data["dir"] + "/" + data["file"] + ":" + str(data["line"])
     else:
-        s += " @ " + module
+        s += " @ " + data["module"]
     return s
 
 
 def formatObj(arr):
     obj = {}
     for i in arr:
-        if i[2] not in obj:
-            obj[i[2]] = {}
-        if i[0]["call"] not in obj[i[2]]:
-            obj[i[2]][i[0]["call"]] = []
-        d = {"call": i[0]["call"], "function": i[1], "module": i[2]}
-        if "line" in i[0] and "file" in i[0] and "dir" in i[0]:
-            d["line"] = i[0]["line"]
-            d["file"] = i[0]["file"]
-            d["dir"] = i[0]["dir"]
-        obj[i[2]][i[0]["call"]].append(d)
+        if i["module"] not in obj:
+            obj[i["module"]] = {}
+        if i["call"] not in obj[i["module"]]:
+            obj[i["module"]][i["call"]] = []
+        d = {"call": i["call"], "function": i["function"], "module": i["module"]}
+        if "line" in i and "file" in i and "dir" in i:
+            d["line"] = i["line"]
+            d["file"] = i["file"]
+            d["dir"] = i["dir"]
+        obj[i["module"]][i["call"]].append(d)
     return obj
 
 
 def analyze(data):
-    if isinstance(data, list):
-        for mod in data:
-            analyze(mod)
+    if not isinstance(data, list):
         return
-    if not isinstance(data, dict):
-        return
-    for module, obj in data.items():
-        for func, calls in obj.items():
-            for callData in calls:
-                if not callData["errno_checked"]:
-                    sev = severity(callData["call"])
-                    if sev == LOW:
-                        low_arr.append((callData, func, module))
-                    elif sev == MED:
-                        med_arr.append((callData, func, module))
-                    elif sev == HIGH:
-                        high_arr.append((callData, func, module))
-                    else:
-                        unk_arr.append((callData, func, module))
+    for item in data:
+        if not item["errno_checked"]:
+            sev = severity(item["call"])
+            if sev == LOW:
+                low_arr.append(item)
+            elif sev == MED:
+                med_arr.append(item)
+            elif sev == HIGH:
+                high_arr.append(item)
+            else:
+                unk_arr.append(item)
 
 
 def main():
@@ -165,22 +159,22 @@ def main():
         if low_arr:
             # print("[ ] Low severity:")
             for i in low_arr:
-                print("LOW: ", formatCallData(*i))
+                print("LOW: ", formatCallData(i))
             low_arr = []
         if med_arr:
             # print("[-] Medium severity:")
             for i in med_arr:
-                print("MED: ", formatCallData(*i))
+                print("MED: ", formatCallData(i))
             med_arr = []
         if high_arr:
             # print("[x] High severity:")
             for i in high_arr:
-                print("HIGH:", formatCallData(*i))
+                print("HIGH:", formatCallData(i))
             high_arr = []
         if unk_arr:
             # print("[?] Unknown severity:")
             for i in unk_arr:
-                print("UNK: ", formatCallData(*i))
+                print("UNK: ", formatCallData(i))
             unk_arr = []
     else:
         root = {}
